66c6d799d9178ed98c366115c0036d1626d48497
Anoosha Naz <163389690+anooshanaz@users.noreply.github.com>
2024-12-19 13:52:43 +0500

    Update FeatureSection.tsx

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,12 +1,11 @@
 
 export default function FeaturesSection() {
-    return (
-        
-      <div className="w-full flex justify-center mb-20">
+    return (   
+      <div className="w-full flex justify-center mb-40">
          <div className="text-center mb-8">
           <h1 className="text-3xl font-semibold text-gray-800">What Make Our Brand Different</h1>
         
-        <div className="max-w-[1320px] w-full h-[244px] bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
+        <div className="max-w-[1320px] w-full lg:h-[24px] md:h-[24px] h-screen  bg-white grid  md:grid-cols-2 lg:grid-cols-4  sm:grid-cols-2 gap-6 p-4 ">
           <div className="flex flex-col items-start text-center md:text-left bg-gray-100 p-10">
             <div className="text-2xl text-cyan-600 mb-2">🚚</div>
             <h3 className="text-lg font-semibold text-cyan-600">Next day as standard</h3>
